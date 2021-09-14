eb7b8099cea3c1cb76a63865718c630bfddc46ab
schernaya <chernaya.sofiia@gmail.com>
2021-09-15 00:20:00 +0300

    add seeds

--- rails-test-task/db/seeds.rb
+++ rails-test-task/db/seeds.rb
@@ -6,3 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+authors = Author.create([
+                          { name: 'Erich Gamma' },
+                          { name: 'Richard Helm' },
+                          { name: 'Ralph Johnson' },
+                          { name: 'John Vlissides' }
+                        ])
+
+p "Created #{Author.count} authors"
+
+Book.create([
+              { title: 'Design Patterns', authors: authors },
+              { title: 'DummyBook2', authors: [authors[1], authors[2]] },
+              { title: 'DummyBook3', authors: [authors[3], authors[1]] }
+            ])
+
+p "Created #{Book.count} books"
